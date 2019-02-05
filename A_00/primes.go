11c02588c319d7d3636752a78f953092e182271e
david_fletcher@taylor.edu <dfletche@cse21716.cs.local>
2019-02-05 15:40:35 -0500

    got go implementation working

--- A_00/primes.go
+++ A_00/primes.go
@@ -1,14 +1,28 @@
 package main
 
-import "fmt"
-import "math"
+import (
+    "fmt"
+    "math"
+    "strings"
+    "strconv"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    fmt.Printf("Enter a number (non-number to quit): ")
 
-    fmt.Println(is_prime(20))
+    var num int
+    // read in user input
+    num_read, err := fmt.Scanf("%d", &num)
 
-    fmt.Println(is_prime(17))
+    // fmt.Printf("num read: %d", num_read)
+
+    if(num_read < 1 && err != nil) {
+        fmt.Println("\n\nBye!")
+    } else {
+        var lst = []int{}
+        prime_partitions(num, 1, lst)
+    }
+
 }
 
 
@@ -25,4 +39,35 @@ func is_prime(n int) (bool) {
         }
     }
     return true
+}
+
+func primes(a int, b int) ([]int) {
+    var primes_list = []int{}
+    for i:=a; i < b + 1; i++ {
+        if(is_prime(i)) {
+            primes_list = append(primes_list, i)
+        }
+    }
+
+    return primes_list
+}
+
+func prime_partitions(n int, k int, lst []int) {
+    if(n == 0){
+        print_primes(lst)
+    } else if(n > k) {
+        for _, p := range primes(k + 1, n) {
+            // fmt.Printf("%d", i)
+            prime_partitions(n - p, p, append(lst, p))
+        }
+    }
+}
+
+func print_primes(lst []int) {
+    print_list := []string{}
+    for i:=0; i < len(lst); i++ {
+        val := strconv.Itoa(lst[i])
+        print_list = append(print_list, val)
+    }
+    fmt.Println(strings.Join(print_list, " + "))
 }
