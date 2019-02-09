63d3c3066f3799d459aeef84580ced24c7860487
Ryan Jones <ryan_jones5@taylor.edu>
2019-02-09 09:29:50 -0500

    Prolog implementation is working

--- A_00/primes.pl
+++ A_00/primes.pl
@@ -1,11 +1,12 @@
 divisible(X,Y) :-
 	N is Y*Y, 						% N=y^2
-	N =< X, 						% and N <= X
+	N =< X, 						% and N <= X (If Y^2 is greater than x,
+									% then Y >= sqrt(X), which is where we end execution in other programs)
 	X mod Y =:= 0. 					% X % Y==0
 
 
 divisible(X,Y) :-
-	Y < X, 							% Confirm that y < x (otherwise not divisible in that order)
+	Y < X, 							% Confirm that y < x (ELSE not divisible in that order)
 	Y1 is Y+1, 						% Used to iterate from [y,x]
 	divisible(X,Y1).				% Run function to check if x is divisible by new y
 
@@ -17,44 +18,59 @@ isprime(X) :-
 
 
 primes(Lo,Hi,L) :-
-	(Lo>Hi -> L=[];					% Hiase case: If Lo > Hi, return empty list
+	(Lo>Hi -> L=[];					% Base case: If Lo > Hi, return empty list
 		(isprime(Lo) ->				% Else: If Lo is a prime number
 				Lo1 is Lo+1,		% Increment Lo
 				primes(Lo1, Hi, X),	% Run primes
 				L = [Lo | X]		% L is a list with Lo added it
-		;							% OTHERWISE
+		;							% ELSE
 			Lo1 is Lo+1,			% Increment Lo
 			primes(Lo1, Hi, L)		% Run primes with new Lo
 		)
 	).
 
 
-isZero(X) :- 						% Is X Zero? Let's find out :thumbs_up:
-	X = 0.
-
-
-list_sum([],0).
+list_sum([],0).						% Base case of List sum: empty list just returns
 list_sum([Head | Tail], TotalSum) :-
-	list_sum(Tail, Sum1),
-	TotalSum is Head + Sum1.
+	list_sum(Tail, Sum),			% Run listsum on next value
+	TotalSum is Head + Sum.		% return sum value (builds from the bottom recursively)
+
+
+print_sum([H | T]) :-
+	T=[] -> 						% If tail is empty (H is last element)
+		write(H),					% Write element to console
+		nl							% Write newline character
+	;								% ELSE
+		write(H),					% Write element
+		write(' + '),				% ... and following '+'
+		print_sum(T).				% Recurse through list'
+
+
+% This function found on the web, how to reverse a list:
+% https://stackoverflow.com/questions/19471778/reversing-a-list-in-prolog
+% Used only for printing our final lists, not used to find solutions
+%
+% Works by appending head of list to another list, which recursion
+% makes a reversed list. Base case returns the new list
+reverse([],Z,Z).					% Base Case: return list
+reverse([H|T],Z,Acc) :-
+	reverse(T,Z,[H|Acc]).			% Append head to Acc
 
 
 prime_partitions(Goal,Primes,List) :-
-	(
-	isZero(Goal) -> print(List), nl;
-		(
-		[H|T] = Primes,
-		H =< Goal ->
-			G is (Goal-H),
-			L is [H|List],
-			prime_partitions(G,T,L)
-		;
-			[H|T] = Primes,
-			prime_partitions(Goal,T,List)
-		)
-	).
+	list_sum(List,Total),			% Get sum of list
+	Total=Goal ->					% If sum(list) == goal
+		reverse(List,L,[]),			% Reverse list to print in ascending order
+		print_sum(L)				% Print_sum function
+	;								% Else
+	Primes=[]->!;					% If primes list is empty end execution
+	[H|T] = Primes,					% Get head, tail from Primes
+	L1 = [H|List],					% Append to new List
+	prime_partitions(Goal,T,L1),	% Recurse with new element in List
+	prime_partitions(Goal,T,List). 	% Recurse without new element in List
 
 
-prime_partitions(Goal) :-
-	primes(2,Goal,L),
-	prime_partitions(Goal,L,[]).
+
+main(Goal) :-
+	primes(2,Goal,L),				% Get Primes list (stores to L)
+	prime_partitions(Goal,L,[]).	% Run prime partitions with given parameters
